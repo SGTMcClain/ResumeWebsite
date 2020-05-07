ff6d78cc52447e69ef2d5f317fe6fb14bf0d8ca6
SGTMcClain <n8mcclain@gmail.com>
2020-05-07 00:44:39 -0400

    add data to app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Job } from './jobs/job';
+import { HttpClient, HttpResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,35 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'resume-website';
+
+  // TODO: Remove after test
+  private jobsUrl = '/api/jobs';
+  data = [];
+
+  constructor(private http: HttpClient) { }
+
+  // get("/api/jobs")
+
+  // Modified for Angular 9 using https://www.positronx.io/angular-promises-example-manage-http-requests/
+  getJobs(){
+    const promise = new Promise((resolve, reject) => {
+      const apiURL = this.jobsUrl;
+      this.http
+        .get<Job[]>(apiURL)
+        .toPromise()
+        .then((res: any) => {
+          // Success
+          this.data = res.map((response: any) => {
+            return response.json() as Job;
+          });
+          resolve();
+        },
+        err => {
+          // Error
+          reject(err);
+        }
+      );
+    });
+    return promise;
+  }
 }
